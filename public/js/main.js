c258c7f53cd407f72524c993bb21fc4856d38dda
Eric Zanchi <emzanchi@gmail.com>
2017-07-20 12:50:08 -0500

    Made it more fun

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,38 @@
-var ws;
-
-if(window.location.protocol === 'http:')
-  ws = new WebSocket(`ws://${window.location.host}`);
-if(window.location.protocol === 'https:')
-  ws = new WebSocket(`wss://${window.location.host}`);
+const ws = window.location.protocol === 'http:' ?
+      new WebSocket(`ws://${window.location.host}`) :
+      new WebSocket(`wss://${window.location.host}`);
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const drawCircle = function drawCircle(x, y){
-  const r = 4;
+const circles = new Set;
+const colors = [
+	'#FF9800',
+  '#FFEB3B',
+  '#4CAF50',
+  '#2196F3',
+  '#9C27B0',
+  '#f44336'
+];
 
-  ctx.beginPath();
-  ctx.ellipse(x, y, r, r, 0, 0, 2 * Math.PI);
-  ctx.fill();
+const drawCircle = function drawCircle(x, y){
+  circles.add({
+    x,
+    y,
+    r: 10,
+    c: randomElement(colors)
+  });
+}
+
+const randomElement = function randomElement(array){
+  return array[Math.floor(Math.random() * array.length)];
 }
 
 var heldDown = false;
 
 canvas.addEventListener('mousedown', function({x, y}){
   heldDown = true;
+  drawCircle(x, y);
 });
 
 canvas.addEventListener('mouseup', function({x, y}){
@@ -31,10 +44,41 @@ canvas.addEventListener('mousemove', function({x, y}){
     ws.send(`${x},${y}`);
     drawCircle(x, y);
   }
-})
+});
+
+var count = 0;
+setInterval(function(){
+  console.log(count);
+  count = 0;
+}, 1000);
 
 ws.onmessage = function({data}){
+  count++;
   const [x, y] = data.split(',');
 
   drawCircle(x, y);
 }
+
+const render = function render(){
+  ctx.clearRect(0, 0, 600, 600);
+
+  for(let circle of circles){
+    let {x, y, r, c} = circle;
+
+    if(r === 100){
+      circles.delete(circle);
+    }
+
+    ctx.strokeStyle = c;
+    ctx.lineWidth = 40 / r;
+    ctx.beginPath();
+    ctx.ellipse(x, y, r, r, 0, 0, Math.PI * 2);
+    ctx.stroke();
+
+    circle.r += r * .05;
+  }
+
+  requestAnimationFrame(render);
+}
+
+render();
